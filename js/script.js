588c040865e5b83e14d5fecbe684ad0c272d17ca
Tohirjon-Odilov <shahanshoh819@gamil.com>
2022-10-06 04:35:21 +0500

    Complete first

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 //! call element
-
 let body = document.body
 let template = body.querySelector('#templete').content
 let tbody = body.querySelector('.tbody')
 let sortEL = body.querySelector('#inputGroupSelect02')
 let search = body.querySelector('#search')
-let modalBtn = body.querySelector('.modal-btn') //! modalni ishga tushirish
+let modalBtn = body.querySelector('#btnModal') //! modalni ishga tushirish
 let result = localStorage.getItem('modal') //! local storage'dan ma'lumot olish
+let img__bg = body.querySelector('.img__bg')
+let count = body.querySelector('#count')
 
 //! add local storage
+// localStorage.removeItem("currencies")
 const localCurrencies = localStorage.getItem("currencies");
-
 let currencies = [];
 
 //! fetch
@@ -21,33 +22,34 @@ async function getMainData(url) {
   try {
     const rawData = await fetch(url);
     const { data } = await rawData.json();
-
     currencies = data;
-
     localStorage.setItem("currencies", JSON.stringify(data));
+
+    //! spinner
+    !rawData.status ? img__bg.classList.toggle('img__btn') : img__bg.className = 'd-none'
   } catch (error) {
     console.error("Internet bilan aloqa yo'qoldi!" + error);
   }
 }
 getMainData("https://pressa-exem.herokuapp.com/api-49");
 
-// console.log(currencies);
-
 //! Create element
+let btnCount;
 function createElement(data) {
   let itemElement = template.cloneNode(true)
 
   itemElement.querySelector(".data__id").textContent = data.Code
   itemElement.querySelector(".data__name").textContent = data.CcyNm_UZ
   itemElement.querySelector(".data__short-name").textContent = data.Ccy
-  itemElement.querySelector(".data__price").textContent = data.Diff
+  itemElement.querySelector(".data__price").textContent = data.Rate
   itemElement.querySelector(".data__date").textContent = data.Date
+  btnCount = itemElement.querySelector("#btnCount")
+
 
   return itemElement
 }
 
 //! render
-
 function render(renderData) {
   tbody.innerHTML = "";
 
@@ -62,20 +64,20 @@ function render(renderData) {
 render(currencies)
 
 // ! sorting data
-// let sorted = null;
 let sortArr = [...currencies]
+let defaultt = currencies
 sortEL.addEventListener('change', function (e) {
   let sortValue = e.target.value
 
   switch (sortValue) {
     case 'down':
-      sortArr.sort((a, b) => a.Diff - b.Diff);
+      sortArr.sort((a, b) => a.Rate - b.Rate);
       break;
     case 'up':
-      sortArr.sort((a, b) => b.Diff - a.Diff);
+      sortArr.sort((a, b) => b.Rate - a.Rate);
       break
     default:
-      sortArr = currencies
+      sortArr = defaultt
       break;
   }
   console.log(sortArr);
@@ -88,17 +90,23 @@ search.addEventListener('input', (e) => {
 
   let newSearch;
   newSearch = [...currencies].filter((searchDate) => {
-    let diff = +searchDate.Diff
-    if (diff >= searchVal) return searchDate
+    if (+searchDate.Rate >= searchVal) return searchDate
   })
   render(newSearch)
 })
-
 
 //! modal
 setTimeout(() => {
   if (!result)
     modalBtn.click()
   localStorage.setItem('modal', 'Ishladi va nihoyat')
-
 }, 10000)
+
+//! bukmark
+btnCount.addEventListener('click', (e) => {
+  console.log(e.target.parentNode)
+
+  if (count >= 0) {
+    count.textContent++
+  }
+})
